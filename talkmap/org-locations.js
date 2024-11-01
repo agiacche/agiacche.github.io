60c5b4c9a007cf3816dd68c272cc786b7504d7b9
agiacche <alessandro.giacchetto@gmail.com>
2024-11-01 18:43:12 +0100

    Talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,127 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+      "Tübingen University, Tübingen, DE",
+      48.5236,
+      9.0522
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+      "University of Leeds, Leeds, GB",
+      53.8067,
+      -1.5530
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+      "IMSA, Miami, US",
+      25.7174,
+      -80.2768
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+      "Monash University, Melbourne, AU",
+      -37.9105,
+      145.1362
+  ],
+  [
+      "University of Melbourne, Melbourne, AU",
+      -37.7963,
+      144.9614
+  ],
+  [
+      "Sorbonne University, Paris, FR",
+      48.8470,
+      2.3553
+  ],
+  [
+      "WWU Münster, Münster, DE",
+      51.9607,
+      7.6261
+  ],
+  [
+      "ETH, Zurich, CH",
+      47.3769,
+      8.5417
+  ],
+  [
+      "LaBRI, Bordeaux, FR",
+      44.8333,
+      -0.6039
+  ],
+  [
+      "IHES, Bures-sur-Yvette, FR",
+      48.6985,
+      2.1727
+  ],
+  [
+      "SISSA, Trieste, IT",
+      45.6519,
+      13.7775
+  ],
+  [
+      "University of Trieste, Trieste, IT",
+      45.6519,
+      13.7768
+  ],
+  [
+      "Universiteit Leiden, Leiden, NL",
+      52.1601,
+      4.4970
+  ],
+  [
+      "USTC, Hefei, CN",
+      31.8369,
+      117.2574
+  ],
+  [
+      "SDU, Odense, DK",
+      55.3680,
+      10.4289
+  ],
+  [
+      "Otranto, IT",
+      40.1484,
+      18.4901
+  ],
+  [
+      "Cergy Paris Université, Cergy, FR",
+      49.0356,
+      2.0706
+  ],
+  [
+      "OIST, Okinawa, JP",
+      26.4670,
+      127.8318
+  ],
+  [
+      "University of Tokyo, Tokyo, JP",
+      35.7130,
+      139.7623
+  ],
+  [
+      "CIRM, Marseille, FR",
+      43.2854,
+      5.4057
+  ],
+  [
+      "EPFL, Lausanne, CH",
+      46.5183,
+      6.5662
+  ],
+  [
+      "University of Edinburgh, Edinburgh, GB",
+      55.9444,
+      -3.1890
+  ],
+  [
+      "ICTP, Trieste, IT",
+      45.7070,
+      13.7267
+  ],
+  [
+      "RIMS, Kyoto, JP",
+      35.0271,
+      135.7809
+  ],
+  [
+      "Obergurgl, AT",
+      46.8711,
+      11.0277
   ]
 ];
